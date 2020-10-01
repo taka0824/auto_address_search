53f7a51d7498729821c6f662ff7d15769666b71b
EC2 Default User <ec2-user@ip-172-31-27-169.ec2.internal>
2020-10-01 02:07:31 +0000

    [Update]Bookers2

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '8638811e33bdff253d974c29bc1d036ab3bc6af25b21f2c8e319fba25f6efd10ceb2723de7050edc20f9715f6876ffad31fa15a4bc352f7e14495bb74068e585'
