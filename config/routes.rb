4381bd3d6ca8f060234d91129ebd2eede2a16e2a
EC2 Default User <ec2-user@ip-172-31-27-169.ec2.internal>
2020-10-20 05:37:39 +0000

    [Finish]follower_followed_function

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
   root "homes#top"
+  post "follow/:id" => "relationships#follow", as: "follow"
+  post "unfollow/:id" => "relationships#unfollow", as: "unfollow"
+
+
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
     resources :post_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :index, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update] do
+    member do
+      get :follower, :followed
+    end
+  end
+
   get 'home/about' => "homes#about"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
